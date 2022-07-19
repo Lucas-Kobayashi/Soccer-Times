d292dc469b192d1bc65800deb976cb1658a23fee
Lucas Kobayashi <kobacarmo2@hotmail.com>
2022-07-19 12:31:32 -0300

    Mock feito

--- app/src/main/java/com/example/soccertimes/ui/news/NewsViewModel.java
+++ app/src/main/java/com/example/soccertimes/ui/news/NewsViewModel.java
@@ -4,16 +4,28 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.soccertimes.domain.News;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class NewsViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<List<News>> news;
 
     public NewsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is News fragment");
+        this.news = new MutableLiveData<>();
+
+//        TODO Remover mock de noticias
+        List<News> news = new ArrayList<>();
+        news.add(new News("Ferroviária tem desfalques importantes", "Lorem Ipsum is simply dummy text of the printing and typesetting industry."));
+        news.add(new News("Ferroviária joga sábado", "Lorem Ipsum is simply dummy text of the printing and typesetting industry."));
+        news.add(new News("Copa do mundo feminina", "Lorem Ipsum is simply dummy text of the printing and typesetting industry."));
+
+        this.news.setValue(news);
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<List<News>> getNews() {
+        return news;
     }
 }
